08cb4ac3a529ce71ebd75042864bf1ca784d0104
denisk13005 <kirmanndenis@gmail.com>
2024-02-12 18:44:54 +0100

    composant home

--- App.js
+++ App.js
@@ -16,5 +16,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#4E4187',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 55
   },
 });
--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import { ScrollView, Text } from 'react-native'
+import React from 'react';
+
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+const homeImage = require('../../assets/homeImage.gif')
 
 export default function Home() {
   return (
-    <ScrollView>
-      <Text>BONJOUR LOIC</Text>
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>Une 📷 Vaut Mieux Qu'une 🚔</Text>
+      <View style= {styles.imageContainer}>
+
+      <Image source={homeImage} style={styles.image}/>
+      </View>
+      <Text style={styles.title}>Prendre une photo d'un horodateur hors service peut vous éviter une amande</Text>
+
+
     </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%', 
+    height: '100%', 
+    padding: 20,
+    
+  },
+  title : {
+    textAlign: 'center', 
+    fontSize: 24,
+    marginTop: 25,
+    marginBottom: 25,
+  },
+  imageContainer:{
+    backgroundColor : 'pink',
+    display: 'flex',
+    alignItems:'center',
+    justifyContent :'center',
+    height : 370,
+    overflow:'hidden',
+    borderRadius : 25
+  },
+  image: {
+    objectFit:'contain', 
+    width : '100%',
+  }
+});
