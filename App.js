095d42747939754f462363e7c27156474c663537
denisk13005 <kirmanndenis@gmail.com>
2024-02-12 17:49:22 +0100

    test deploy react native

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import Home from './components/Home/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Home />
     </View>
   );
 }
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4E4187',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ScrollView, Text } from 'react-native'
+
+export default function Home() {
+  return (
+    <ScrollView>
+      <Text>BONJOUR LOIC</Text>
+    </ScrollView>
+  )
+}
